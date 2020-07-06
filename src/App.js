3e68fc8b871dab88936a78a29accea911fb3b30d
xWesko <heyluisd@gmail.com>
2020-07-05 21:38:09 -0700

    sidebar y router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
 
+
+//Paginas
+import Ordenes from './components/paginas/Ordenes';
+import Menu from './components/paginas/Menu';
+import NuevoPlatillo from './components/paginas/NuevoPlatillo';
+
+//Componentes UI
+import Sidebar from './components/ui/Sidebar';
 
 function App() {
     return ( 
-        <h1 className="text-3xl font-light"> Hola mundo </h1>
+        <div className="md:flex min-h-screen ">
+            <Sidebar />
+
+            <div className="md:w-3/5 xl:w-4/5">
+                <Routes>
+                    <Route path="/" element={ <Ordenes/>  } /> 
+                    <Route path="/menu" element={ <Menu/>  } />
+                    <Route path="/nuevo-platillo" element={ <NuevoPlatillo/>  } />
+                </Routes>
+            </div>
+
+       
+        </div>
     );
 }
 
+
+
 export default App;
+
+
---
+++ src/components/ui/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink  } from 'react-router-dom';
+
+const Sidebar = () => {
+
+    return(
+        <>
+            <div className="md:w-2/5 xl:w-1/5 bg-gray-800">
+                <div className="p-6">
+                    <p className="uppercase text-white text-2xl tracking-wide text-center font-bold"> RestaurantApp </p>
+
+                    <p className="mt-3 text-gray-600"> Administra tu restaurant en las siguientes opciones: </p>
+                    
+                    <nav className="mt-10">
+                        <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" exact activeClassName="text-yellow-500" to="/"> Ordenes </NavLink>
+                        <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" exact activeClassName="text-yellow-500" to="/menu"> Menú </NavLink>
+                    </nav>
+
+                </div>
+            </div>
+        </>
+
+
+    );
+
+}
+
+export default Sidebar;
